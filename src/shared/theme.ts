b185dde6b9d09b489c2cc945b2124316b840d878
Gabriel Linhares <glinharesb@gmail.com>
2022-08-16 20:30:30 -0300

    fix: ajust font in chakra theme

--- src/shared/theme.ts
+++ src/shared/theme.ts
@@ -1,6 +1,10 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
+  fonts: {
+    body: `'Roboto', sans-serif`,
+    heading: `'Roboto', sans-serif`
+  },
   components: {
     Button: {
       variants: {
